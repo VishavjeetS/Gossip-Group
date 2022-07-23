6202a595772ea1a3eade9a03a72bc2ad31a36839
VishavjeetS <vishavhanspal00@gmail.com>
2022-07-24 02:46:05 +0530

    Authentication Completed

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,10 +1,7 @@
 const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
 
 const generateToken = (id) => {
-  const secret = process.env.JWT_SECRET;
-  return jwt.sign({ id }, secret, {
+  return jwt.sign({ id }, "secret", {
     expiresIn: "30d",
   });
 };
